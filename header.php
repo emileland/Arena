6c9b7b45a9230fc5f78fcf791ecb2290058c43ac
Emile Landerretche <emile@landerretche.net>
2024-02-21 11:33:02 +0100

    create files

---
+++ header.php
@@ -0,0 +1,7 @@
+<nav>
+<a href="index.php">Arena</a>
+    <ul>
+        <li>test</li>
+        <li>2eme</li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Arena - Page d'Accueil</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <?php require_once(__DIR__ . '/header.php'); ?>
+    </body>
+</html>
